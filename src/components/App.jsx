ae3f050b4d52d48940c33fdbfe4222003b9adabe
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-09 19:31:25 -0500

    Successfully updating parent state with a callback bound in the child; working on getting the child to rerender props without having to update state internally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import TopPanel from './TopPanel';
 
-
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.setUserName = this.setUserName.bind(this);
+        this.state = {
+            userName: null
+        };
+    }
+
+    setUserName(name) {
+        this.setState({userName: name});
+    }
+
     render() {
         return (
             <Container>
-                <TopPanel />
+                <TopPanel setUserName={this.setUserName} user={this.state.userName}/>
             </Container>
         );
     }
--- src/components/TopPanel.jsx
+++ src/components/TopPanel.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Swal from 'sweetalert2';
 
 class TopPanel extends Component {
     constructor(props) {
@@ -10,18 +11,32 @@ class TopPanel extends Component {
         this.state = {
             isLoggedIn: false,
             buttonOneText: 'Login',
-            buttonTwoText: 'Sign up'
+            buttonTwoText: 'Sign up',
+            user: this.props.userName
         }
         this.initialState = this.state;
     }
 
+    async handleSignupDelete() {
+        if (!this.state.isLoggedIn) {
+            const { value: name } = await Swal.fire({
+                title: 'Input username',
+                input: 'text',
+                inputPlaceholder: 'Enter your desired username'
+            });
+            if (name) {
+                this.props.setUserName(name);
+            }
+        }
+        this.handleLoginLogout();
+    }
+
     handleLoginLogout() {
-        console.log(this.state);
         if (!this.state.isLoggedIn) {
             this.setState({
                 isLoggedIn: true,
-                buttonOneText: 'Explore',
-                buttonTwoText: 'Logout'
+                buttonOneText: 'Logout',
+                buttonTwoText: `Delete @${this.state.user}`
             });
         } else {
             this.setState(this.initialState);
@@ -32,7 +47,7 @@ class TopPanel extends Component {
         return (
             <Row style={{marginTop: 10+'px'}} className="justify-content-end">
                 <Col md={2}><Button onClick={() => this.handleLoginLogout()} className="badge-pill" variant="outline-info">{this.state.buttonOneText}</Button></Col>
-                <Col md={2}><Button onClick={() => this.handleLoginLogout()} className="badge-pill" variant="info">{this.state.buttonTwoText}</Button></Col>
+                <Col md={2}><Button onClick={() => this.handleSignupDelete()} className="badge-pill" variant="info">{this.state.buttonTwoText}</Button></Col>
             </Row>
         );
     }
