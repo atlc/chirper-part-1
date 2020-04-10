42cce9be7b8ce460bca5c0145660d1d298e4030f
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-09 20:08:06 -0500

    Updated naming convention for the top login panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import TopPanel from './TopPanel';
+import LoginPanel from './LoginPanel';
 
 class App extends Component {
     constructor(props) {
@@ -19,7 +19,7 @@ class App extends Component {
     render() {
         return (
             <Container>
-                <TopPanel setUserName={this.setUserName} />
+                <LoginPanel setUserName={this.setUserName} />
             </Container>
         );
     }
