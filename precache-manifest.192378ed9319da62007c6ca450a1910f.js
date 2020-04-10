7af6e7cdb5d1b4ae76db35138c0dfd49902ae5af
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-10 03:43:43 -0500

    Updates

---
+++ precache-manifest.192378ed9319da62007c6ca450a1910f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84db58dcd5902606a5c15c87e4f8889",
+    "url": "/chirper-part-1/index.html"
+  },
+  {
+    "revision": "be0c99c6159c849e85f7",
+    "url": "/chirper-part-1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df2c79d195ba5fd38eed",
+    "url": "/chirper-part-1/static/css/main.58738d32.chunk.css"
+  },
+  {
+    "revision": "be0c99c6159c849e85f7",
+    "url": "/chirper-part-1/static/js/2.e2cbcb46.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chirper-part-1/static/js/2.e2cbcb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2c79d195ba5fd38eed",
+    "url": "/chirper-part-1/static/js/main.bfeff795.chunk.js"
+  },
+  {
+    "revision": "3d2e808e6067cb39462a",
+    "url": "/chirper-part-1/static/js/runtime-main.b38daa3a.js"
+  }
+]);
